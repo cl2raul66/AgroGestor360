af059de4fa6f7961913f96b9ad92e749786ffbf9
Raul Avila Catala <cl2raul66_dev_gt@hotmail.com>
2024-03-15 06:58:43 -0600

    vista básica para artículos y productos

--- src/AgroGestor360.App/AgroGestor360.App/ViewModels/Settings/CvProductsViewModel.cs
+++ src/AgroGestor360.App/AgroGestor360.App/ViewModels/Settings/CvProductsViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,4 +10,28 @@ namespace AgroGestor360.App.ViewModels.Settings;
 
 public partial class CvProductsViewModel : ObservableObject
 {
+    [ObservableProperty]
+    bool isProductsVisible;
+
+    [RelayCommand]
+    void ViewArticles()
+    {
+        IsProductsVisible = false;
+    }
+
+    [RelayCommand]
+    void ViewProducts()
+    {
+        IsProductsVisible = true;
+    }
+
+    [RelayCommand]
+    async Task AddCategory()
+    {
+        string resul = await Shell.Current.DisplayPromptAsync("Agregar categoría", "Nombre:", "Agregar", "Cancelar", "Escriba aquí");
+        if (string.IsNullOrEmpty(resul))
+        {
+            return;
+        }
+    }
 }
